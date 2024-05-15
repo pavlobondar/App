e10a9a38152b107f8bc71c225ebf546f237e1b54
Pavlo Bondar <ivizey@gmail.com>
2024-05-15 08:54:48 +0300

    Initial Commit

---
+++ WApp/WAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAppApp.swift
+//  WApp
+//
+//  Created by Pavlo on 15.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct WAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
